7bc2eafab46dad38ad60e7951629c249d742ae63
Dávid Karnok <akarnokd@gmail.com>
2017-10-28 15:09:05 +0200

    Fix concurrency issues.

--- src/main/java/com/example/operators/StreamPublisher.java
+++ src/main/java/com/example/operators/StreamPublisher.java
@@ -1,6 +1,6 @@
 package com.example.operators;
 
-import java.util.Iterator;
+import java.util.*;
 import java.util.concurrent.Flow;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
@@ -17,70 +17,108 @@ public class StreamPublisher<T> implements Flow.Publisher<T> {
 
     @Override
     public void subscribe(Flow.Subscriber<? super T> subscriber) {
-        StreamSubscription subscription = new StreamSubscription(subscriber);
+        Iterator<? extends T> iterator = null;
+
+        try {
+            iterator = streamSupplier.get().iterator();
+        } catch (Throwable e) {
+            subscriber.onSubscribe(new Flow.Subscription() {
+                @Override
+                public void request(long n) {
+
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+            subscriber.onError(e);
+            return;
+        }
+
+        StreamSubscription subscription = new StreamSubscription(subscriber, iterator);
         subscriber.onSubscribe(subscription);
-        subscription.doOnSubscribed();
     }
 
     private class StreamSubscription implements Flow.Subscription {
         private final Flow.Subscriber<? super T> subscriber;
         private final Iterator<? extends T> iterator;
-        private final AtomicBoolean isTerminated = new AtomicBoolean(false);
+        private final AtomicBoolean isTerminated = new AtomicBoolean();
         private final AtomicLong demand = new AtomicLong();
         private final AtomicReference<Throwable> error = new AtomicReference<>();
 
-        StreamSubscription(Flow.Subscriber<? super T> subscriber) {
+        StreamSubscription(Flow.Subscriber<? super T> subscriber, Iterator<? extends T> iterator) {
             this.subscriber = subscriber;
-            Iterator<? extends T> iterator = null;
-
-            try {
-                iterator = streamSupplier.get().iterator();
-            } catch (Throwable e) {
-                error.set(e);
-            }
-
             this.iterator = iterator;
         }
 
         @Override
         public void request(long n) {
-            if (n <= 0 && !terminate()) {
-                subscriber.onError(new IllegalArgumentException("negative subscription request"));
-                return;
+            if (n <= 0) {
+                error.compareAndSet(null, new IllegalArgumentException("negative subscription request"));
+                n = 1;
             }
 
-            if (demand.get() > 0) {
-                demand.getAndAdd(n);
-                return;
-            }
-
-            demand.getAndAdd(n);
-
-            for (; demand.get() > 0 && iterator.hasNext() && !isTerminated(); demand.decrementAndGet()) {
-                try {
-                    subscriber.onNext(iterator.next());
-                } catch (Throwable e) {
-                    if (!terminate()) {
-                        subscriber.onError(e);
+            for (;;) {
+                long r = demand.get();
+                long u = r + n;
+                if (u < 0L) {
+                    u = Long.MAX_VALUE;
+                }
+                if (demand.compareAndSet(r, u)) {
+                    if (r != 0L) {
+                        return;
                     }
+                    break;
                 }
             }
 
-            if (!iterator.hasNext() && !terminate()) {
-                subscriber.onComplete();
+            for (;;) {
+                if (error.get() != null) {
+                    subscriber.onError(error.get());
+                    return;
+                }
+
+                long e = 0L;
+
+                while (e != n) {
+                    if (isTerminated()) {
+                        return;
+                    }
+
+                    boolean has = false;
+                    T value = null;
+
+                    try {
+                        has = iterator.hasNext();
+                        if (has) {
+                            value = iterator.next();
+                        }
+                    } catch (Throwable ex) {
+                        subscriber.onError(ex);
+                        return;
+                    }
+
+                    if (!has) {
+                        subscriber.onComplete();
+                        return;
+                    }
+
+                    subscriber.onNext(value);
+                    e++;
+                }
+
+                n = demand.addAndGet(-e);
+                if (n == 0) {
+                    break;
+                }
             }
         }
 
         @Override
         public void cancel() {
             terminate();
-        }
-
-        void doOnSubscribed() {
-            Throwable throwable = error.get();
-            if (throwable != null && !terminate()) {
-                subscriber.onError(throwable);
-            }
         }
 
         private boolean terminate() {
